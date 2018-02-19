ad92edb6d4c9a96e1284e1abe36cf87348d9c176
leonardom249 <leonardo.megan@gmail.com>
2018-02-19 14:08:39 -0600

    Final Set One

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 function whoAmI(name, age) {
     console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
+    if(!name || !age){
+        console.error("Arguments not valid");
+    }
+    if (typeof name !== String){
+        console.error("Name not valid");
+    }
+    if (typeof age !== Number){
+        console.error("Number not valid");
+    }
+
 };
 
 function yearOfBirth(age){
     let yob = 2018 - age;
-    console.log(yob);
+    if (age<0) {
+        throw new Error ("Age can not be negative");
+    }
+    else{
+        console.log(yob);
+    }
+}
+
+try{
+    console.log(yearOfBirth(100));
+    console.log(yearOfBirth(-1));
+    console.log(yearOfBirth());
+} catch(e){
+    console.log(e.message);
 };
 
 whoAmI('Anya', 101);
-yearOfBirth(101);
+yearOfBirth(80);
+whoAmI("Megan");
+whoAmI(92,90);
+whoAmI("Ted", "red");
